1040bc2e206bcb0db1cabb0eba074b9c0846f11e
Yuuki Sone <yuki_sone@dwango.co.jp>
2019-04-09 19:11:02 +0900

    Update cloud-init things

--- packer/scripts/00_mount_volume.sh
+++ packer/scripts/00_mount_volume.sh
@@ -4,6 +4,6 @@ cat <<CFG | sudo tee /etc/cloud/cloud.cfg.d/15_mount_volume.cfg
 #cloud-config
 mounts:
   - [ "/dev/sdb", "/minecraft", "ext4", "defaults,nofail", "0", "2" ]
-runcmd:
-  - sudo rm -rf /var/lib/cloud/* && sudo cloud-init init --local && sudo cloud-init init && sudo cloud-init modules --mode config && sudo cloud-init modules --mode final
 CFG
+
+sudo rm -rf /var/lib/cloud/*
